415c17bf72f6bb93ff71c0e9ee60e3817e4d976e
Vijay Paruchuri <gamebo11@duck.com>
2024-03-20 18:44:37 -0400

    Created the Layer_Dense class and added Forward Method

--- Dense.py
+++ Dense.py
@@ -1,2 +1,23 @@
 import numpy as np
 
+# X = np.random.rand(3,4)
+# y = np.array([[0,0,1], [1,0,0], [0,1,0]])
+
+
+# The base Dense layer with a forward and a backward method
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.rand(n_inputs, n_neurons)
+        self.biases = np.zeros(n_neurons)
+
+    def forward(self, X):
+        self.output = np.dot(X, self.weights) + self.biases
+
+# dense1 = Layer_Dense(4, 4)
+#
+# dense2 = Layer_Dense(4, 3)
+#
+# dense1.forward(X)
+# dense2.forward(dense1.output)
+#
+# print(dense2.output)
