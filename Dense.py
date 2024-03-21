c88acfbc64429024a6f4b0349d3a6a89d6864d5c
Vijay Paruchuri <gamebo11@duck.com>
2024-03-21 03:13:24 -0400

    Fixed the biases array shape

--- Dense.py
+++ Dense.py
@@ -1,23 +1,10 @@
-# import numpy as np
-
-# X = np.random.rand(3,4)
-# y = np.array([[0,0,1], [1,0,0], [0,1,0]])
-
+import numpy as np
 
 # The base Dense layer with a forward and a backward method
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.rand(n_inputs, n_neurons)
-        self.biases = np.zeros(n_neurons)
+        self.biases = np.zeros([1, n_neurons])
 
     def forward(self, X):
         self.output = np.dot(X, self.weights) + self.biases
-
-# dense1 = Layer_Dense(4, 4)
-
-# dense2 = Layer_Dense(4, 3)
-
-# dense1.forward(X)
-# dense2.forward(dense1.output)
-
-# print(dense2.output)
