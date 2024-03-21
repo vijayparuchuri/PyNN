7ff6b520402f749a90f4b0ab6fd1d2356d480851
Vijay Paruchuri <gamebo11@duck.com>
2024-03-21 03:14:11 -0400

    Instantiated Activation_ReLU object

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from sklearn.datasets import load_iris
 import pandas as pd
 import numpy as np
 from Dense import Layer_Dense
-
+from ReLU import Activation_ReLU
 
 iris = load_iris()
 
@@ -13,13 +13,15 @@ df['targets'] = iris.target
 X = df.drop('targets', axis=1).values
 y = df['targets'].values
 
+# Forward Pass
 
-dense1  = Layer_Dense(4, 64)
+#Initializations
+dense1  = Layer_Dense(4, 4)
+dense2 = Layer_Dense(4, 3)
+activation1 = Activation_ReLU()
 
-dense2 = Layer_Dense(64, 3)
-
+#Running the forward pass
 dense1.forward(X)
-
 dense2.forward(dense1.output)
-
-print(dense2.output)
+activation1.forward(dense2.output)
+print(activation1.output)
