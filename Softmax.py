b9f2a629e87755fb5b19fc37bdbcc0d9d237fcef
Vijay Paruchuri <gamebo11@duck.com>
2024-03-22 01:09:15 -0400

    Fixed the Forward method in Activation_Softmax class

--- Softmax.py
+++ Softmax.py
@@ -3,6 +3,6 @@ import numpy as np
 # Softmax activation class
 class Activation_Softmax:
     def forward(self, input):
-        samples = len(input)
+        input = input-np.max(input, axis=1, keepdims=1)
         exponetiated = np.exp(input)
         self.output = exponetiated/np.sum(exponetiated, axis=1, keepdims=True)
