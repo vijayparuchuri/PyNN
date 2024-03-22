63b9c9638310896f54cd1dfc55106c4341bb3e56
Vijay Paruchuri <gamebo11@duck.com>
2024-03-22 01:09:42 -0400

    Added loss object to calculate and print out current loss

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import numpy as np
 from Dense import Layer_Dense
 from ReLU import Activation_ReLU
 from Softmax import Activation_Softmax
+from CategoricalCrossEntropy import Loss_CategoricalCrossEntropy
 
 iris = load_iris()
 
@@ -21,13 +22,11 @@ dense1  = Layer_Dense(4, 4)
 dense2 = Layer_Dense(4, 3)
 activation1 = Activation_ReLU()
 activation2 = Activation_Softmax()
+loss = Loss_CategoricalCrossEntropy()
 
 #Running the forward pass
 dense1.forward(X)
 activation1.forward(dense1.output)
 dense2.forward(activation1.output)
 activation2.forward(dense2.output)
-
-
-print(dense2.output)
-print(activation2.output)
+print(loss.calculate(activation2.output, y))
