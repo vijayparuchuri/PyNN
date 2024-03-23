eef21527537ee2ab837959c3d29e961e270939e3
Vijay Paruchuri <gamebo11@duck.com>
2024-03-23 01:53:35 -0400

    Created Backward method to calculate the partial Derivative for ReLU function.

--- ReLU.py
+++ ReLU.py
@@ -3,6 +3,8 @@ import numpy as np
 #The base ReLU activation class
 class Activation_ReLU:
     def forward(self, input):
+        self.input = input
         self.output = np.maximum(0, input)
-
-
+    def backward(self, dvalue):
+        self.dinputs = self.dvlaues.copy()
+        self.dinputs[self.dinputs < 0] = 0
