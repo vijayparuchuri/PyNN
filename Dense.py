c0b89de7ca9b51e71570f048d705dce18d85cce7
Vijay Paruchuri <gamebo11@duck.com>
2024-03-23 01:50:56 -0400

    Created Backward method to calculate the gradients for weights, inputs, biases

--- Dense.py
+++ Dense.py
@@ -7,4 +7,11 @@ class Layer_Dense:
         self.biases = np.zeros([1, n_neurons])
 
     def forward(self, X):
+        self.input = X
         self.output = np.dot(X, self.weights) + self.biases
+
+    def  backward(self, dvalues):
+        self.dinputs = np.dot(dvalues, self.weights.T)
+        self.dweights = np.dot(inputs.T, dvalues)
+        self.dweights = np.sum(dvalues, axis=0, keepdims=True)
+
