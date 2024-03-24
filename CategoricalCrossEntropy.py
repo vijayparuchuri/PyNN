c5587f4249dcd38c8e4d4ab028fdb732a1dba1a7
Vijay Paruchuri <gamebo11@duck.com>
2024-03-24 19:26:31 -0400

    Created 'Backward' method with Categorical CrossEntropy derivative code for Chain Rule

--- CategoricalCrossEntropy.py
+++ CategoricalCrossEntropy.py
@@ -11,8 +11,16 @@ class Loss_CategoricalCrossEntropy(Loss):
         y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
         if len(y_true.shape) == 1:
             correct_confidences = y_pred_clipped[range(samples), y_true]
-        elif len(y_true.shape):
+        elif len(y_true.shape) == 2:
             correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
         negative_log_likelyhood = -np.log(correct_confidences)
         return negative_log_likelyhood
+        
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        labels = len(dvalues[0])
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+        self.dinputs = - y_true / dvalues
+        self.dinputs = self.dinputs / samples
         
