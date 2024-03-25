f7968e93051efa69aecc6f8e00cc7d98a13c52c0
Vijay Paruchuri <gamebo11@duck.com>
2024-03-25 11:16:35 -0400

    Updated some variable names

--- ReLU.py
+++ ReLU.py
@@ -5,6 +5,6 @@ class Activation_ReLU:
     def forward(self, input):
         self.input = input
         self.output = np.maximum(0, input)
-    def backward(self, dvalue):
-        self.dinputs = self.dvlaues.copy()
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
         self.dinputs[self.dinputs < 0] = 0
