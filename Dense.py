a0a1864f9cf4af5be046024d29e9a5d1371a7149
Vijay Paruchuri <gamebo11@duck.com>
2024-03-25 11:12:02 -0400

    Fixed some issues in the 'Backward' method implementation

--- Dense.py
+++ Dense.py
@@ -6,12 +6,12 @@ class Layer_Dense:
         self.weights = 0.10 * np.random.rand(n_inputs, n_neurons)
         self.biases = np.zeros([1, n_neurons])
 
-    def forward(self, X):
-        self.input = X
-        self.output = np.dot(X, self.weights) + self.biases
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
 
     def  backward(self, dvalues):
         self.dinputs = np.dot(dvalues, self.weights.T)
-        self.dweights = np.dot(inputs.T, dvalues)
-        self.dweights = np.sum(dvalues, axis=0, keepdims=True)
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
 
