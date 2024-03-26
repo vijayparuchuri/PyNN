4cfba35d08bac289982f03e19f86aaf4a7cf294c
Vijay Paruchuri <gamebo11@duck.com>
2024-03-26 14:06:11 -0400

    Updated the 'Backward' method initializations.

--- Main.py
+++ Main.py
@@ -6,24 +6,24 @@ from ReLU import Activation_ReLU
 from Softmax import Activation_Softmax
 from CategoricalCrossEntropy import Loss_CategoricalCrossEntropy
 
-# iris = load_iris()
+iris = load_iris()
 
-# df = pd.DataFrame(data = iris.data, columns=iris.feature_names)
+df = pd.DataFrame(data = iris.data, columns=iris.feature_names)
 
-# df['targets'] = iris.target
+df['targets'] = iris.target
 
-# X = df.drop('targets', axis=1).values
-# y = df['targets'].values
-X = np.array([[1, 2, 3, 2.5],
-[2., 5., -1., 2],
-[-1.5, 2.7, 3.3, -0.8]])
-y = np.array([[0,0,1], [1,0,0], [0,1,0]])
+X = df.drop('targets', axis=1).values
+y = df['targets'].values
+# X = np.array([[1, 2, 3, 2.5],
+# [2., 5., -1., 2],
+# [-1.5, 2.7, 3.3, -0.8]])
+# y = np.array([[0,0,1], [1,0,0], [0,1,0]])
 
 # Forward Pass
 
 #Initializations
-dense1  = Layer_Dense(4, 3)
-dense2 = Layer_Dense(3, 3)
+dense1  = Layer_Dense(4, 64)
+dense2 = Layer_Dense(64, 3)
 activation1 = Activation_ReLU()
 activation2 = Activation_Softmax()
 loss = Loss_CategoricalCrossEntropy()
@@ -35,16 +35,10 @@ dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 print(loss.calculate(activation2.output, y))
 
-dvalues = np.array([[1., 1., 1.],
-                    [2., 2., 2.],
-                    [3., 3., 3.]])
-#Backward Pass
 
-activation1.backward(dvalues)
+#Backward Pass
+loss.backward(activation2.output, y)
+activation2.backward(loss.dinputs)
+dense2.backward(activation2.dinputs)
+activation1.backward(dense2.dinputs)
 dense1.backward(activation1.dinputs)
-print(dense1.weights)
-print(dense1.biases)
-dense1.weights += - 0.001 * dense1.dweights
-dense1.biases += -0.001 * dense1.dbiases
-print(dense1.weights)
-print(dense1.biases)
