dbe9da3e92ea0c9d81ab442bb480da24c7913b2e
Vijay Paruchuri <gamebo11@duck.com>
2024-03-31 20:45:09 -0400

    Updated weights initialization to avoid Gradient Explosion

--- Dense.py
+++ Dense.py
@@ -3,7 +3,7 @@ import numpy as np
 # The base Dense layer with a forward and a backward method
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10 * np.random.rand(n_inputs, n_neurons)
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros([1, n_neurons])
 
     def forward(self, inputs):
