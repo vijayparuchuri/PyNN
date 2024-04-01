15cedd0fd58d34143e15a90058bc2fa824753ee6
Vijay Paruchuri <gamebo11@duck.com>
2024-03-31 20:42:27 -0400

    Changed dinputs initialization using np.zeros_like method to np.empty_like method

--- Softmax.py
+++ Softmax.py
@@ -3,11 +3,12 @@ import numpy as np
 # Softmax activation class
 class Activation_Softmax:
     def forward(self, input):
-        input = input-np.max(input, axis=1, keepdims=1)
+        self.input = input
+        input = input-np.max(input, axis=1, keepdims=True)
         exponetiated = np.exp(input)
         self.output = exponetiated/np.sum(exponetiated, axis=1, keepdims=True)
     def backward(self, dvalues):
-        self.dinputs = np.zeros_like(self.output)
+        self.dinputs = np.empty_like(dvalues)
         for index, (single_output, single_dvalue) in enumerate(zip(self.output, dvalues)):
             single_output = single_output.reshape(-1, 1)
             jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
