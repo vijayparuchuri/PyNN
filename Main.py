5d7d8baee4b49ec31369e87818daf4aef8a06248
Vijay Paruchuri <gamebo11@duck.com>
2024-04-02 11:57:10 -0400

    Updated Main.py with optimizer code, now the Dense network can run end to end on Iris dataset with a Training Loss of 0.040 and Accuracy of 0.987.

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ from ReLU import Activation_ReLU
 from Softmax import Activation_Softmax
 from CategoricalCrossEntropy import Loss_CategoricalCrossEntropy
 from ActivationLossDerivative import Activation_Softmax_Loss_CategoricalCrossEntropy
+from Optimizers import SGD, AdaGrad, RMSProp, Adam
 
 iris = load_iris()
 
@@ -15,6 +16,24 @@ df['targets'] = iris.target
 
 X = df.drop('targets', axis=1).values
 y = df['targets'].values
+
+# def spiral_data(points, classes):
+#     X = np.zeros((points * classes, 2))
+#     y = np.zeros(points * classes, dtype="uint8")
+#     for class_number in range(classes):
+#         ix = range(points * class_number, points * (class_number + 1))
+#         r = np.linspace(0.0, 1, points)  # radius
+#         t = (
+#             np.linspace(class_number * 4, (class_number + 1) * 4, points)
+#             + np.random.randn(points) * 0.2
+#         )
+#         X[ix] = np.c_[r * np.sin(t * 2.5), r * np.cos(t * 2.5)]
+#         y[ix] = class_number
+#     return X, y
+
+
+# X, y = spiral_data(100, 3)
+
 # X = np.array([[1, 2, 3, 2.5],
 # [2., 5., -1., 2],
 # [-1.5, 2.7, 3.3, -0.8]])
@@ -26,26 +45,33 @@ y = df['targets'].values
 dense1  = Layer_Dense(4, 64)
 dense2 = Layer_Dense(64, 3)
 activation1 = Activation_ReLU()
-activation2 = Activation_Softmax()
-loss = Loss_CategoricalCrossEntropy()
 activation_loss = Activation_Softmax_Loss_CategoricalCrossEntropy()
+# optimizer = SGD(decay=1e-3, momentum=0.99, learning_rate=.1)
+# optimizer = AdaGrad(0.1, decay=1e-4, epsilon=1e-4)
+# optimizer = RMSProp(0.02, decay=1e-5, rho=0.9, epsilon=1e-7)
+optimizer = Adam(learning_rate=0.05, decay=5e-7)
 
 #Running the forward pass
-dense1.forward(X)
-activation1.forward(dense1.output)
-dense2.forward(activation1.output)
-activation2.forward(dense2.output)
-# print(loss.calculate(activation2.output, y))
-print(activation_loss.forward(dense2.output, y))
-
+for i in range(10001):
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    loss = activation_loss.forward(dense2.output, y)
+    predictions = np.argmax(activation_loss.output, axis = 1)
+    if len(y.shape) == 2:
+        y = np.argmax(y, axis=1)
+    accuracy = np.mean(predictions == y)
+    if i % 100 == 0:
+        print(f'Epoch: {i}....Accuracy: {accuracy:.3f}....Loss: {loss:.3f}....lr: {optimizer.current_learning_rate:.3f}')
 
 #Backward Pass
-activation_loss.backward(activation2.output, y)
-# loss.backward(activation2.output, y)
-# activation2.backward(loss.dinputs)
-dense2.backward(activation_loss.dinputs)
-activation1.backward(dense2.dinputs)
-dense1.backward(activation1.dinputs)
+    activation_loss.backward(activation_loss.output, y)
+    dense2.backward(activation_loss.dinputs)
+    activation1.backward(dense2.dinputs)
+    dense1.backward(activation1.dinputs)
 
 
-print(dense1.dweights)
+    optimizer.pre_update_params()
+    optimizer.update_params(dense1)
+    optimizer.update_params(dense2)
+    optimizer.post_update_params()
