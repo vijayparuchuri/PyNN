953b1d85f9416bedb5238f9b4e63d9c9dafef925
Vijay Paruchuri <paruchurivijay40@gmail.com>
2024-06-30 14:49:11 -0400

    Updated the Main file with initializations for binary classifaction

--- Main.py
+++ Main.py
@@ -1,13 +1,17 @@
 from sklearn.datasets import load_iris
+from keras.api.losses import BinaryCrossentropy
 import pandas as pd
 import numpy as np
 from Layers.Dense import Layer_Dense
 from Layers.Dropout import Layer_Dropout
 from Activations.ReLU import Activation_ReLU
 from Activations.Softmax import Activation_Softmax
+from Activations.Sigmoid import Activation_Sigmoid
 from Losses.CategoricalCrossEntropy import Loss_CategoricalCrossEntropy
+from Losses.BinaryCrossEntropy import Loss_BinaryCrossEntropy
 from Losses.ActivationLossDerivative import Activation_Softmax_Loss_CategoricalCrossEntropy
 from Optimziers.Optimizers import SGD, AdaGrad, RMSProp, Adam
+from nnfs.datasets import spiral_data
 
 # iris = load_iris()
 
@@ -18,22 +22,9 @@ from Optimziers.Optimizers import SGD, AdaGrad, RMSProp, Adam
 # X = df.drop('targets', axis=1).values
 # y = df['targets'].values
 
-def spiral_data(points, classes):
-    X = np.zeros((points * classes, 2))
-    y = np.zeros(points * classes, dtype="uint8")
-    for class_number in range(classes):
-        ix = range(points * class_number, points * (class_number + 1))
-        r = np.linspace(0.0, 1, points)  # radius
-        t = (
-            np.linspace(class_number * 4, (class_number + 1) * 4, points)
-            + np.random.randn(points) * 0.2
-        )
-        X[ix] = np.c_[r * np.sin(t * 2.5), r * np.cos(t * 2.5)]
-        y[ix] = class_number
-    return X, y
+X, y = spiral_data(100, 2)
 
-
-X, y = spiral_data(100, 3)
+y = y.reshape(-1, 1)
 
 # X = np.array([[1, 2, 3, 2.5],
 # [2., 5., -1., 2],
@@ -43,15 +34,18 @@ X, y = spiral_data(100, 3)
 # Forward Pass
 
 #Initializations
-dense1  = Layer_Dense(2, 64, weight_regularizer_L2 = 5e-4, bias_regularizer_L2=5e-4)
+dense1  = Layer_Dense(2, 256, weight_regularizer_L2 = 5e-4, bias_regularizer_L2=5e-4)
 dropout = Layer_Dropout(0.1)
-dense2 = Layer_Dense(64, 3)
+dense2 = Layer_Dense(256, 1)
 activation1 = Activation_ReLU()
-activation_loss = Activation_Softmax_Loss_CategoricalCrossEntropy()
+activation2 = Activation_Sigmoid()
+loss_function = Loss_BinaryCrossEntropy()
+loss_sk = BinaryCrossentropy(from_logits=False)
+# activation_loss = Activation_Softmax_Loss_CategoricalCrossEntropy()
 # optimizer = SGD(decay=1e-3, momentum=0.99, learning_rate=.1)
 # optimizer = AdaGrad(0.1, decay=1e-4, epsilon=1e-4)
 # optimizer = RMSProp(0.02, decay=1e-5, rho=0.9, epsilon=1e-7)
-optimizer = Adam(learning_rate=0.005, decay=1e-5)
+optimizer = Adam(learning_rate=0.01, decay=1e-4)
 
 #Running the forward pass
 for i in range(10001):
@@ -59,19 +53,30 @@ for i in range(10001):
     activation1.forward(dense1.output)
     dropout.forward(activation1.output)
     dense2.forward(activation1.output)
-    data_loss = activation_loss.forward(dense2.output, y)
-    regularization_loss = activation_loss.loss.regularization_loss(dense1) + activation_loss.loss.regularization_loss(dense2)
+    activation2.forward(dense2.output)
+    data_loss = loss_function.calculate(activation2.output, y)
+    
+    
+    # data_loss = activation_loss.forward(dense2.output, y)
+    regularization_loss = loss_function.regularization_loss(dense1) + loss_function.regularization_loss(dense2)
+    
     loss = data_loss + regularization_loss
-    predictions = np.argmax(activation_loss.output, axis = 1)
-    if len(y.shape) == 2:
-        y = np.argmax(y, axis=1)
+    predictions = (activation2.output > 0.5) * 1
     accuracy = np.mean(predictions == y)
     if i % 100 == 0:
-        print(f'Epoch: {i}....Accuracy: {accuracy:.3f}....Loss: {loss:.3f} (data_loss:{data_loss:.3f}...reg_loss:{regularization_loss:.3f})....lr: {optimizer.current_learning_rate:.3f}')
+        print(f'Epoch: {i}....Loss: {loss:.3f} (data_loss: {data_loss:.3f}....reg_loss: {regularization_loss:.3f})....Accuracy: {accuracy}')
+    # predictions = np.argmax(activation_loss.output, axis = 1)
+    # if len(y.shape) == 2:
+    #     y = np.argmax(y, axis=1)
+    # accuracy = np.mean(predictions == y)
+    # if i % 100 == 0:
+    #     print(f'Epoch: {i}....Accuracy: {accuracy:.3f}....Loss: {loss:.3f} (data_loss:{data_loss:.3f}...reg_loss:{regularization_loss:.3f})....lr: {optimizer.current_learning_rate:.3f}')
 
 #Backward Pass
-    activation_loss.backward(activation_loss.output, y)
-    dense2.backward(activation_loss.dinputs)
+    # activation_loss.backward(activation_loss.output, y)
+    loss_function.backward(activation2.output, y)
+    activation2.backward(loss_function.dinputs)
+    dense2.backward(activation2.dinputs)
     dropout.backward(dense2.dinputs)
     activation1.backward(dense2.dinputs)
     dense1.backward(activation1.dinputs)
@@ -83,14 +88,18 @@ for i in range(10001):
     optimizer.post_update_params()
 
     ############Validation dataset##############
-    X, y = spiral_data(100, 3)
-    dense1.forward(X)
+    X_test, y_test = spiral_data(100, 2)
+    y_test = y_test.reshape(-1, 1)
+    dense1.forward(X_test)
     activation1.forward(dense1.output)
     dense2.forward(activation1.output)
-    loss =  activation_loss.forward(dense2.output, y)
-    predictions = np.argmax(activation_loss.output, axis=1)
-    if len(y.shape) == 2:
-        y = np.argmax(y, axis=1)
-    accuracy = np.mean(predictions==y)
+    activation2.forward(dense2.output)
+    dloss =  loss_function.calculate(activation2.output, y)
+    predictions = (activation2.output > 0.5) * 1
+    accuracy = np.mean(predictions == y_test)
+    # predictions = np.argmax(activation_loss.output, axis=1)
+    # if len(y.shape) == 2:
+    #     y = np.argmax(y, axis=1)
+    # accuracy = np.mean(predictions==y)
     if i % 100 == 0:
-        print(f'Validation Accuracy:{accuracy:.4f}, Validation Loss:{loss:.4f}')
+        print(f'Validation Accuracy:{accuracy:.4f}, Validation Loss:{dloss:.4f}')
