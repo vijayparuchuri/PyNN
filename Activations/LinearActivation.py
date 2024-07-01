3c2a0581dc6e527ecc543004f06bf5482c49de92
Vijay Paruchuri <paruchurivijay40@gmail.com>
2024-07-01 12:35:13 -0400

    Created a Linear Activation class

---
+++ Activations/LinearActivation.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class Activation_Linear:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = inputs
+    
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
