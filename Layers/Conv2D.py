cd8b64e4d02b2975eafdc1f60c0d7202495408da
Vijay Paruchuri <paruchurivijay40@gmail.com>
2024-07-05 14:22:35 -0400

    Updated the Layer_Conv2D class to use scipy's convolve2d method to massively speedup convolution calculations in both forward and backward pass. Making the code is much more consise and readable

--- Layers/Conv2D.py
+++ Layers/Conv2D.py
@@ -10,11 +10,10 @@ class Layer_Conv2D:
         self.biases = np.zeros(1)
         
     def forward(self, input):
-        if self.padding == 'valid':
-            self.input = input
-            self.output = np.zeros((input.shape[0] - self.k_h + 1, input.shape[1] - self.k_w + 1))
-            
-        elif self.padding == 'same':
+        self.input = input
+        self.output = convolve2d(self.input, np.flipud(np.fliplr(self.weights)), mode=self.padding, fillvalue=0) + self.biases
+        
+        if self.padding == 'same':
             if self.k_h % 2 == 1 and self.k_h % 2 == 1:
                 self.p_h = (self.k_h - 1) // 2
                 self.p_w = (self.k_w - 1) // 2
@@ -26,25 +25,10 @@ class Layer_Conv2D:
                 self.p_w_left = (self.k_w - 1) // 2
                 self.p_w_right = self.k_w // 2
                 
-        
             self.input = np.pad(input, pad_width=((self.p_h_top, self.p_h_bottom), 
                                           (self.p_w_left, self.p_w_right)), mode='constant')
-            
-            self.output = np.zeros((input.shape[0], input.shape[0]))
-        else:
-            raise ValueError('Padding must be "valid" or "same".')
-        for row in range(self.output.shape[0]):
-            for column in range(self.output.shape[1]):
-                sub_array = self.input[row:row+self.k_h, column:column+self.k_w]
-                self.output[row, column] = np.sum(sub_array*self.weights) + self.biases
-            
     def backward(self, dvalues):
-        self.dweights = np.zeros_like(self.weights)
-        self.dinputs = np.zeros_like(self.input, dtype=float)
-        for i in range(dvalues.shape[0]):
-            for j in range(dvalues.shape[1]):
-                self.dweights+= self.input[i:i+self.k_h, j:j+self.k_w] * dvalues[i, j]
-                self.dinputs[i:i+self.k_h, j:j+self.k_w] += self.weights * dvalues[i, j]
-        if self.padding == 'same':
-            self.dinputs = self.dinputs[self.p_h_top:-self.p_h_bottom, self.p_w_left:-self.p_w_right]
+        self.dweights = convolve2d(self.input, np.flipud(np.fliplr(dvalues)), mode='valid')
+        self.dinputs = convolve2d(dvalues, np.flipud(np.fliplr(self.weights)),mode=self.padding)
+    
         self.dbiases = np.sum(dvalues, axis=(0, 1))
