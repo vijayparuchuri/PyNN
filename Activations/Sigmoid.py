c2142953394b3bf2db6b9920f33da29fb00c1e6e
Vijay Paruchuri <paruchurivijay40@gmail.com>
2024-07-05 10:58:11 -0400

    Added Predictions method to Activation_Sigmoid class.

--- Activations/Sigmoid.py
+++ Activations/Sigmoid.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Activation_Sigmoid:
     
-    def forward(self, inputs):
+    def forward(self, inputs, training):
         self.inputs = inputs
         exponentiated_inputs = np.exp(-inputs)
         self.output = 1 / (1 + exponentiated_inputs)
@@ -10,3 +10,5 @@ class Activation_Sigmoid:
     def backward(self, dvalues):
         self.dinputs = dvalues  * (1 - self.output) * self.output
         
+    def predictions(self, outputs):
+        return (outputs > 0.5) * 1
