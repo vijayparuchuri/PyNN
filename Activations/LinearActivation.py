dff423eead64baa430c0513f3489910678db3d5d
Vijay Paruchuri <paruchurivijay40@gmail.com>
2024-07-05 10:57:13 -0400

    Added prediction method.

--- Activations/LinearActivation.py
+++ Activations/LinearActivation.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 class Activation_Linear:
-    def forward(self, inputs):
+    def forward(self, inputs, training):
         self.inputs = inputs
         self.output = inputs
     
     def backward(self, dvalues):
         self.dinputs = dvalues.copy()
+        
+    def predictions(self, outputs):
+        return outputs
